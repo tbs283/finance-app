72eb873d858e73c70759d9455fa8106cb22ab313
tbs283 <2017firstisorion@gmail.com>
2021-10-27 15:31:08 +0000

    top画面修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,22 +2,20 @@ class HomesController < ApplicationController
   def top
     @blog_ranks = Blog.find(Favorite.group(:blog_id).order('count(blog_id) desc').limit(9).pluck(:blog_id))
     @new_blog = Blog.all.order(created_at: :desc)
-    if user_signed_in? && Goal.exists?
-      @last_goal = Goal.where(user_id: current_user.id).last
-      # @days_left = (@last_goal.goal_limit - Date.current-1).to_i
-      @incomes = current_user.balances.where(balance: 0).sum(:amount)
-      @expenses = current_user.balances.where(balance: 1).sum(:amount)
-      @savings = @incomes - @expenses
-      if @last_goal.goal_amount - @savings.to_i < 0
-        redirect_to goals_select_path
-      else
-        @to_goal = @last_goal.goal_amount - @savings.to_i
-      end
-      @to_goal = @last_goal.goal_amount.to_i - @savings.to_i
-    elsif user_signed_in?
-      @incomes = current_user.balances.where(balance: 0).sum(:amount)
-      @expenses = current_user.balances.where(balance: 1).sum(:amount)
-      @savings = @incomes - @expenses
+    if user_signed_in?
+      @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
+      @balances = current_user.balances
+      @balance_new = Balance.new
+      @incomes = @balances.where(balance: 0, period: @month.all_month)#その月の収入
+      @expenses = @balances.where(balance: 1, period: @month.all_month)#その月の支出
+      @income_sum = @incomes.sum(:amount)#その月の収入の合計
+      @expense_sum = @expenses.sum(:amount) #その月の支出の合計
+      @balance = @income_sum-@expense_sum #その月の収支
+      @incomes_all = @balances.where(balance: 0)#収入全体
+      @expenses_all = @balances.where(balance: 1)#支出全体
+      @income_all_sum = @incomes_all.sum(:amount)#収入全体の合計
+      @expense_all_sum = @expenses_all.sum(:amount)#支出全体の合計
+      @saving = @income_all_sum-@expense_all_sum#貯金額
     else
     end
   end
