7e4d9ca6a8c5919d350fb312771d4546bba64ba6
tbs283 <2017firstisorion@gmail.com>
2021-11-19 15:38:32 +0000

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
   resources :users, :only => [:show, :edit, :update]
   resources :balances, :only => [:edit, :create, :index, :show, :update, :destroy] do
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
+  resources :users, only: :show
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'homes#top'
