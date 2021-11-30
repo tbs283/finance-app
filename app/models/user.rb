d60fc9facab90eda86a4231a883d8b586a0c0f6d
tbs283 <2017firstisorion@gmail.com>
2021-11-30 10:25:47 +0000

    “デプロイ11/30”

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :budgets, dependent: :destroy
   has_many :blogs, dependent: :destroy
   has_many :comments, dependent: :destroy
