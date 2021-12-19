75bf8b56c8e45db55a4e1ea372eed6ca46101fe1
tbs283 <2017firstisorion@gmail.com>
2021-12-19 14:01:20 +0000

    フォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,10 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:following_id])
     current_user.follow(@user)
+    render :create
   end
 
   def destroy
     @user = User.find(params[:id])
     current_user.unfollow(@user)
+    render :destroy
   end
 end
